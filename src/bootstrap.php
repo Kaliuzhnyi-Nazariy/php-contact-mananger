fdb283842f1146d83993b9e05ae1a1f333ad27ed
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-05-28 12:03:12 +0200

    change load

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -2,7 +2,7 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-Dotenv\Dotenv::createUnsafeImmutable(__DIR__ . '/../')->safeLoad();
+Dotenv\Dotenv::createImmutable(__DIR__ . '/../')->safeLoad();
 
 session_start();
 
