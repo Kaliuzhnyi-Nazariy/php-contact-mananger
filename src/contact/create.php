b303c226655d5fc60a02888351ef34d20dd9b647
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-05-28 11:22:40 +0200

    update create contact logic

--- src/contact/create.php
+++ src/contact/create.php
@@ -4,6 +4,9 @@
 
 $uploadsFolder = __DIR__ . '/../../public/uploads/';
 
+require __DIR__ . '/../lib/cloudinary.php';
+// $cloudinary = require __DIR__ . '/../lib/cloudinary.php';
+
 $errors = [];
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -13,26 +16,28 @@ $email = filter_input(INPUT_POST, "email", FILTER_VALIDATE_EMAIL);    $phone = t
     $photo = '';
 
 
+
     if (empty($name) || empty($email) || empty($phone)) {
         $errors[] = 'Name, email and phone fields are required!';
     }
 
     if ($name && $email && $phone) {
         if(isset($_FILES['photo'])) {
-            if (!is_dir($uploadsFolder)) {
-                mkdir($uploadsFolder,0777, true);
-            }
+            // if (!is_dir($uploadsFolder)) {
+            //     mkdir($uploadsFolder,0777, true);
+            // }
 
             $imageName = time() . '_' . basename($_FILES['photo']['name']);
             $imagePath = $uploadsFolder . $imageName;
 
-            try {
-             move_uploaded_file($_FILES['photo']['tmp_name'], $imagePath);
-                 $photo = "/uploads/" . $imageName;
-             }
-             catch (Exception $e) {
-                return null;
-            }
+            $photo = uploadImage($_FILES['photo']);
+            // try {
+            //  move_uploaded_file($_FILES['photo']['tmp_name'], $imagePath);
+            //      $photo = "/uploads/" . $imageName;
+            //  }
+            //  catch (Exception $e) {
+            //     return null;
+            // }
         }
 
         $stmt = $pdo->prepare('INSERT INTO contacts (name, email, phone, photo, owner_id) VALUES (:name, :email, :phone, :photo, :owner_id);');
