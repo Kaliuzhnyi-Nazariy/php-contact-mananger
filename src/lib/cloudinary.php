7a28d872e775ec90d21636faab4b5211e6ce2a0c
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-05-28 11:20:25 +0200

    add cloudinary

---
+++ src/lib/cloudinary.php
@@ -0,0 +1,19 @@
+<?php
+use Cloudinary\Configuration\Configuration;
+
+$cloudinary = $_ENV["CLOUDINARY_LINK"];
+
+Configuration::instance($cloudinary);
+
+use Cloudinary\Api\Upload\UploadApi;
+
+function uploadImage(mixed $file) {
+    $upload = new UploadApi();
+
+    $res = $upload->upload($file['tmp_name'], [
+        'folder' => 'php_crm',
+        'public_id' => 'contact_' . $file['name'] . '_' .time()
+    ]);
+
+    return $res['secure_url'];
+}
