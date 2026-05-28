ab87d2cfdc4b11387d9e4b87f72d5ed189d3356c
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-05-28 11:21:26 +0200

    update bootstrap

--- public/home.php
+++ public/home.php
@@ -1,7 +1,5 @@
 <?php
 
-require '../vendor/autoload.php';
-
 // session_start();
 
 // $pdo = require __DIR__ .'/../src/db.php';
--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,5 +1,11 @@
 <?php
 
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+
+$dotenv->load();
+
 session_start();
 
 $pdo = require __DIR__ ."/../src/db.php";
